0e7f7b8554f55170dd3133741ba3c571624928f6
Cqwrr <116123523+Cqwrr@users.noreply.github.com>
2025-02-09 12:51:27 +0400

    new file:   cmd/add.go 	modified:   go.mod 	new file:   go.sum 	modified:   internal/expenses/expenses.go 	new file:   internal/expenses/expenses.json

---
+++ cmd/add.go
@@ -0,0 +1,6 @@
+package main
+
+import (
+	"github.com/cqwrr/expenseTracker"
+	"github.com/spf13/cobra"
+)
--- internal/expenses/expenses.go
+++ internal/expenses/expenses.go
@@ -1,7 +1,8 @@
-package expenses
+package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"time"
 )
@@ -14,39 +15,90 @@ type expense struct {
 	Description string    `json:"description"`
 }
 
-var expenses []expense
-var filePath = "expenses.json"
-
-func saveExpenses() error {
-	data, err := json.Marshal(expenses)
-	if err != nil {
-		return err
-	}
-	if err := os.WriteFile(filePath, data, 0644); err != nil {
-		return err
-	}
-	return nil
-}
-func loadExpenses() error {
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			expenses = []expense{}
-			return nil
-		}
-		return err
-	}
-	return json.Unmarshal(data, &expenses)
+func main() {
+	loadExpenses()
+	editExpense(2, 100, "food", "Aaaaaaaaaaaaaaaaaaa", nil)
 }
 
-func addExpense(Amount float64, Category string, Description string, Date time.Time) expense {
-	id := len(expenses) + 1
-	exp := expense{Id: id, Amount: Amount, Category: Category, Date: Date, Description: Description}
-	expenses = append(expenses, exp)
+func newExpense(id int, amount float64, category string, date time.Time, description string) *expense {
+	return &expense{
+		Id:          id,
+		Amount:      amount,
+		Category:    category,
+		Date:        time.Now(),
+		Description: description,
+	}
+}
+
+var expenses []*expense // Глобальный слайс трат
+func AddExpense(amount float64, category string, description string) *expense {
+	var newExpenseId int
+	if len(expenses) == 0 {
+		newExpenseId = 1
+	} else {
+		newExpenseId = expenses[len(expenses)-1].Id + 1
+	}
+
+	exp := newExpense(newExpenseId, amount, category, time.Now(), description)
+	expenses = append(expenses, exp) //сохранение в слайс трат
 	saveExpenses()
 	return exp
 }
 
-func getExpenses() []expense {
-	return expenses
+func saveExpenses() {
+	f, err := os.Create("expenses.json")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(expenses)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadExpenses() error {
+	f, err := os.Open("expenses.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = json.NewDecoder(f).Decode(&expenses)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func delExpenses(id int) (*expense, error) {
+	for i, exp := range expenses {
+		if exp.Id == id {
+			deletedExp := exp
+			expenses = append(expenses[:i], expenses[i+1:]...)
+			for j := i; j < len(expenses); j++ {
+				expenses[j].Id = j + 1
+			}
+			saveExpenses()
+			return deletedExp, nil
+		}
+	}
+	return nil, fmt.Errorf("Трата с id %d не найдена", id)
+}
+
+func editExpense(id int, amount float64, category string, description string, date *time.Time) (*expense, error) {
+	for _, exp := range expenses {
+		if exp.Id == id {
+			exp.Amount = amount
+			exp.Category = category
+			exp.Description = description
+			if date != nil {
+				exp.Date = *date
+			}
+			saveExpenses()
+			return exp, nil
+		}
+	}
+	return nil, fmt.Errorf("Трата с id %d не найдена", id)
 }
